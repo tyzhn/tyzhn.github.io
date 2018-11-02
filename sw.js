5c0fbb626c49d6af9f19a836d3103f7925aef551
tyzhn <44608577+tyzhn@users.noreply.github.com>
2018-11-02 18:10:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@ self.addEventListener('activate', function(event) {
           if (expectedCacheNames.indexOf(cacheName) === -1) {
             // If this cache name isn't present in the array of "expected" cache names, then delete it.
             console.log('Deleting out of date cache:', cacheName);
-            return caches.delete(cacheName);
+            //return caches.delete(cacheName);
           }
         })
         );
