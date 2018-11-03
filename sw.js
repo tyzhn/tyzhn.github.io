2dbc332c3294107391bd08e72ff68718f79d807b
tyzhn <44608577+tyzhn@users.noreply.github.com>
2018-11-03 11:03:22 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,143 +1,112 @@
-var CURRENT_CACHES = {
-  prefetch: 'sw-example'
+(function() {
+  var CURRENT_CACHES = {
+  prefetch: 'com.interbiz.ptsdhelp'
 };
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   var urlsToPrefetch = [
     './'
-    //'index.js',
-    //'../../styles/main.css',
-    //'static/poster.jpg',
-    // The videos are stored remotely with CORS enabled.
-    //'https://prefetch-video-sample.storage.googleapis.com/gbike.webm',
-    //'https://prefetch-video-sample.storage.googleapis.com/gbike.mp4'
   ];
-
-
-  // All of these logging statements should be visible via the "Inspect" interface
-  // for the relevant SW accessed via chrome://serviceworker-internals
 
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
-      return cache.addAll(urlsToPrefetch);
+    caches.open(CURRENT_CACHES.prefetch).then(function (cache) {
+        return cache.addAll(urlsToPrefetch);
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  // While there is only one cache in this example, the same logic will handle the case where
-  // there are multiple versioned caches.
-  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
-    return CURRENT_CACHES[key];
+self.addEventListener('activate', function (event) {
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+      return CURRENT_CACHES[key];
   });
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (expectedCacheNames.indexOf(cacheName) === -1) {
-            // If this cache name isn't present in the array of "expected" cache names, then delete it.
-            console.log('Deleting out of date cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-        );
+    caches.keys().then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.map(function (cacheName) {
+              if (expectedCacheNames.indexOf(cacheName) === -1) {
+                  return caches.delete(cacheName);
+              }
+          })
+          );
     })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
+self.addEventListener('fetch', function (event) {
   if (event.request.headers.get('range')) {
-    var pos =
-    Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-    console.log('Range request for', event.request.url,
-      ', starting position:', pos);
-    event.respondWith(
-      caches.open(CURRENT_CACHES.prefetch)
-      .then(function(cache) {
-        return cache.match(event.request.url);
-      }).then(function(res) {
-        if (!res) {
-          return fetch(event.request)
-          .then(res => {
+      var pos =
+      Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
+      event.respondWith(
+        caches.open(CURRENT_CACHES.prefetch)
+        .then(function (cache) {
+            return cache.match(event.request.url);
+        }).then(function (res) {
+            if (!res) {
+                return fetch(event.request)
+                .then(res => {
+                    return res.arrayBuffer();
+                });
+            }
             return res.arrayBuffer();
-          });
-        }
-        return res.arrayBuffer();
-      }).then(function(ab) {
-        return new Response(
-          ab.slice(pos),
-          {
-            status: 206,
-            statusText: 'Partial Content',
-            headers: [
-              ['Content-Type', 'video/mp4'],
-              ['Content-Range', 'bytes ' + pos + '-' +
-                (ab.byteLength - 1) + '/' + ab.byteLength]]
-          });
-      }));
+        }).then(function (ab) {
+            return new Response(
+              ab.slice(pos),
+              {
+                  status: 206,
+                  statusText: 'Partial Content',
+                  headers: [
+                    // ['Content-Type', 'video/webm'],
+                    ['Content-Range', 'bytes ' + pos + '-' +
+                      (ab.byteLength - 1) + '/' + ab.byteLength]]
+              });
+        }));
   } else {
-    console.log('Non-range request for', event.request.url);
-    event.respondWith(
-    // caches.match() will look for a cache entry in all of the caches available to the service worker.
-    // It's an alternative to first opening a specific named cache and then matching on that.
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found response in cache:', response);
-        return response;
-      }
-      console.log('No response found in cache. About to fetch from network...');
-      // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
-      // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
-      return fetch(event.request).then(function(response) {
-        console.log('Response from network is:', response);
-      console.log(event.request.url);
-      if(event.request.url == 'https://tyzhn.github.io/pages/a.html')
-      {
-        caches.open(CURRENT_CACHES.prefetch)
-                .then(cache =>
-                 {  console.log('add page a to cache');
-                    cache.add('https://tyzhn.github.io/small.mp4');
-                });
-      }
-      else if(event.request.url == 'https://tyzhn.github.io/pages/b.html')
-      {
-        caches.open(CURRENT_CACHES.prefetch)
-                .then(cache =>
-                 {  console.log('add page b to cache');
-                    cache.add('https://tyzhn.github.io/vb.mp4');
-                });
-      }
-      else if(event.request.url == 'https://tyzhn.github.io/pages/c.html')
-      {
-        caches.open(CURRENT_CACHES.prefetch)
-                .then(cache =>
-                 {  console.log('add page c to cache');
-                    cache.add('https://tyzhn.github.io/vc.mp4');         
-                });
-      }
+      event.respondWith(
+      caches.match(event.request).then(function (response) {
+          if (response) {
+              return response;
+          }
+          return fetch(event.request).then(function (response) {
+              if (event.request.url == 'http://tyzhn.github.io/pages/a.html') {
+                  caches.open(CURRENT_CACHES.prefetch)
+                          .then(cache => {
+                              cache.add('http://tyzhn.github.io/small.mp4');
+                          });
+              }
+              else if (event.request.url == 'http://tyzhn.github.io/pages/b.html') {
+                  caches.open(CURRENT_CACHES.prefetch)
+                          .then(cache => {
+                              cache.add('http://tyzhn.github.io/vb.mp4');
+                          });
+              }
+              else if (event.request.url == 'http://tyzhn.github.io/pages/b.html') {
+                  caches.open(CURRENT_CACHES.prefetch)
+                          .then(cache => {
+                              cache.add('http://tyzhn.github.io/vc.mp4');
+                          });
+              }
 
-        return caches.open(CURRENT_CACHES.prefetch)
-                .then(cache =>
-                 {  console.log('add new page to cache');
-                    cache.put(event.request.url ,response.clone());
-                    return response;
-                });
-   
-      }).catch(function(error) {
-        // This catch() will handle exceptions thrown from the fetch() operation.
-        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-        // It will return a normal response object that has the appropriate error code set.
-        console.error('Fetching failed:', error);
+              else {
+                  console.log("Page not cached");
+              }
+              
+              
+         
+              return caches.open(CURRENT_CACHES.prefetch)
+                      .then(cache => {
+                          cache.put(event.request.url, response.clone());
+                          return response;
+                      });
 
-        throw error;
-      }); 
-    })
-    );
+          }).catch(function (error) {
+              console.error('Fetching failed:', error);
+              throw error;
+          });
+      })
+      );
   }
 });
+})();
